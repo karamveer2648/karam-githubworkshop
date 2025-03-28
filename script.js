06ed454c1a243ea4251a08b5ecf88d5f9e96b1d9
Karamveer Singh Oberoi <71765505+karamveer2648@users.noreply.github.com>
2025-03-28 17:05:11 +0530

    created html file

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example: Form submission handler
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message!');
+});
